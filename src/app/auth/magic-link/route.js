60909f105f2c5d7f60f4d78285bcbaa9fe9d7de0
valeriikharkin <brafifaabraka@gmail.com>
2025-08-28 09:19:39 -0700

    feat: add functionality to recovery password

--- src/app/auth/magic-link/route.js
+++ src/app/auth/magic-link/route.js
@@ -1,56 +1,55 @@
-// import { getSupabaseCookiesUtilClient } from "@/supabase-utils/cookiesUtilClient"
-import { NextResponse } from "next/server"
-import { getSupabaseAdminClient } from "@/supabase-utils/adminClient"
-import nodemailer from "nodemailer"
+import { getSupabaseAdminClient } from "@/supabase-utils/adminClient";
+import nodemailer from "nodemailer";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const formData = await request.formData()
-  const email = formData.get("email")
-  // const supabase = getSupabaseCookiesUtilClient()
-  const supabaseAdmin = getSupabaseAdminClient()
-  
-  // const { data, error } = await supabase.auth.signInWithOtp({
-  //   email,
-  //   options: { shouldCreateUser: false }
-  // })
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const supabaseAdmin = getSupabaseAdminClient();
+  const type = formData.get("type") === "recovery" ? "recovery" : "magiclink";
 
-  const { data: linkData, error } = await supabaseAdmin.auth.admin.generateLink({
-    email,
-    type: "magiclink"
-  })
+  const { data: linkData, error } = await supabaseAdmin.auth.admin.generateLink(
+    {
+      email,
+      type,
+    },
+  );
 
   if (error) {
     return NextResponse.redirect(
-      new URL("/error?type=magiclink", request.url),
-      302
-    )
+      new URL(`/error?type=${type}`, request.url),
+      302,
+    );
   }
 
-  const { hashed_token } = linkData.properties
+  const { hashed_token } = linkData.properties;
+
   const constructedLink = new URL(
-    `/auth/verify?hashed_token=${hashed_token}`,
-    request.url
-  )
+    `/auth/verify?hashed_token=${hashed_token}&type=${type}`,
+    request.url,
+  );
+
   const transporter = nodemailer.createTransport({
     host: "localhost",
     port: 54325,
-  })
+  });
+
+  const initialSentence =
+    type === "recovery"
+      ? "Hi there, you requested a password change!"
+      : "Hi there, this is a custom magic link email!";
+  const secondSentenceEnding = type === "recovery" ? "change it" : "log in";
 
   await transporter.sendMail({
     from: "Your Company <your@mail.whatever>",
     to: email,
     subject: "Magic Link",
     html: `
-            <h1>Hi there, this is a custom magic link email!</h1>
-            <p>Click <a href="${constructedLink.toString()}">here</a> to log
-            in.</p>
-          `,
-  })
+    <h1>${initialSentence}</h1>
+    <p>Click <a href="${constructedLink.toString()}">here</a> to ${secondSentenceEnding}.</p>
+    `,
+  });
 
-  // const thanksUrl = new URL("/magic-thanks", request.url)
-  // return NextResponse.redirect(thanksUrl, 302)
-  return NextResponse.redirect(
-    new URL("/magic-thanks", request.url), 
-    302
-  )
+  const thanksUrl = new URL(`/magic-thanks?type=${type}`, request.url);
+  return NextResponse.redirect(thanksUrl, 302);
 }
--- src/supabase-utils/adminClient.js
+++ src/supabase-utils/adminClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const getSupbaseAdminClient = () => {
+export const getSupabaseAdminClient = () => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
