d0bc1ec813f597d0703f38e97d79cc332389c669
valeriikharkin <brafifaabraka@gmail.com>
2025-09-04 07:31:41 -0700

    feat: add functionality to create new ticket

--- src/supabase-utils/cookiesUtilClient.js
+++ src/supabase-utils/cookiesUtilClient.js
@@ -2,26 +2,29 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const getSupabaseCookiesUtilClient = () => {
-  const cookieStore = cookies()
+  const cookieStore = cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
+      auth: {
+        flowType: "pkce",
+        autoRefreshToken: false,
+      },
+
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
+        async getAll() {
+          return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        async setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options)
-            })
-          } catch (err) {
-            console.error("Failed to set cookies", err);
-          }
-        }
-      }
-    }
-  )
-}
+              cookieStore.set(name, value, options);
+            });
+          } catch {}
+        },
+      },
+    },
+  );
+};
