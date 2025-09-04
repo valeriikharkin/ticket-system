beba9e49b3ad7caa3c460bce412ed353ad0e365c
valeriikharkin <brafifaabraka@gmail.com>
2025-09-04 16:54:15 -0700

    feat: add viewing the ticket details with DB

--- src/app/[tenant]/tickets/details/[id]/page.js
+++ src/app/[tenant]/tickets/details/[id]/page.js
@@ -1,21 +1,44 @@
 import classes from "./TicketDetails.module.css";
 import { TicketComments } from "./TicketComponents";
+import { getSupabaseCookiesUtilClient } from "@/supabase-utils/cookiesUtilClient";
+import { notFound } from "next/navigation";
+import { TICKET_STATUS } from "@/utils/constants";
 
-export default function TicketDetailsPage({ params }) {
+export default async function TicketDetailsPage({ params }) {
+
+  const supabase = getSupabaseCookiesUtilClient()
+  const id = Number(params.id)
+  const { data : ticket, error } = await supabase
+    .from("tickets")
+    .select("*")
+    .eq("id", id)
+    .single()
+
+  if (error) return notFound()
+
+  const {
+    created_at,
+    title,
+    description,
+    created_by,
+    status,
+    author_name
+  } = ticket
+
+  const dateString = new Date(created_at).toLocaleDateString("en-US")
+
   return (
     <article className={classes.ticketDetails}>
       <header>
-        <strong># {params.id}</strong> - <strong className={classes.ticketStatusGreen}>Open</strong>
+        <strong># {id}</strong> - <strong className={classes.ticketStatusGreen}>{TICKET_STATUS[status]}</strong>
         <br />
         <small className={classes.authorAndDate}>
-          Created by <strong>AuthorName</strong> at {"  "}
-          <time>December 10th 2025</time>
+          Created by <strong>{author_name}</strong> at {"  "}
+          <time>{dateString}</time>
         </small>
-        <h2>Ticket title should be here</h2>
+        <h2>{title}</h2>
       </header>
-      <section>
-        Some details about the ticket should be here
-      </section>
+      <section>{description}</section>
 
       <TicketComments />
     </article>
