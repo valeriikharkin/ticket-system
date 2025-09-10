6d01be98d78e0bf278a3edcd8fbd9837b773c029
valeriikharkin <brafifaabraka@gmail.com>
2025-09-10 08:09:27 -0700

    feat: add functionality and UI to delete tickets by tenant user

--- src/app/[tenant]/tickets/details/[id]/page.js
+++ src/app/[tenant]/tickets/details/[id]/page.js
@@ -1,47 +1,42 @@
-import classes from "./TicketDetails.module.css";
-import { TicketComments } from "./TicketComponents";
 import { getSupabaseCookiesUtilClient } from "@/supabase-utils/cookiesUtilClient";
 import { notFound } from "next/navigation";
-import { TICKET_STATUS } from "@/utils/constants";
+import { TicketDetails } from "./TicketDetails";
+import { headers } from "next/headers";
 
 export default async function TicketDetailsPage({ params }) {
-
-  const supabase = getSupabaseCookiesUtilClient()
-  const id = Number(params.id)
-  const { data : ticket, error } = await supabase
+  const supabase = getSupabaseCookiesUtilClient();
+  const id = Number(params.id);
+  const { data: ticket, error } = await supabase
     .from("tickets")
     .select("*")
     .eq("id", id)
-    .single()
+    .single();
 
-  if (error) return notFound()
+  if (error) return notFound();
 
-  const {
-    created_at,
-    title,
-    description,
-    created_by,
-    status,
-    author_name
-  } = ticket
+  const supabase_user_id = (await supabase.auth.getUser()).data.user.id;
+  const { data: serviceUser } = await supabase
+    .from("service_users")
+    .select("id")
+    .eq("supabase_user", supabase_user_id)
+    .single();
 
-  const dateString = new Date(created_at).toLocaleDateString("en-US")
+  const isAuthor = serviceUser.id === ticket.created_by;
+  const { created_at, title, description, created_by, status, author_name } =
+    ticket;
+
+  const dateString = new Date(created_at).toLocaleString("en-US");
 
   return (
-    <article className={classes.ticketDetails}>
-      <header>
-        <strong># {id}</strong> - <strong className={classes.ticketStatusGreen}>{TICKET_STATUS[status]}</strong>
-        <br />
-        <small className={classes.authorAndDate}>
-          Created by <strong>{author_name}</strong> at {"  "}
-          <time>{dateString}</time>
-        </small>
-        <h2>{title}</h2>
-      </header>
-      <section>{description}</section>
-
-      <TicketComments />
-    </article>
-
-  )
+    <TicketDetails
+      id={id}
+      tenant={ticket.tenant}
+      title={title}
+      description={description}
+      status={status}
+      author_name={author_name}
+      dateString={dateString}
+      isAuthor={isAuthor}
+    />
+  );
 }
